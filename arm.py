e262384fb1ea9e71493431b178a86d89c8937ad6
Connor <cweatherly19@bsmschool.org>
2018-09-11 14:23:18 -0500

    first commit

---
+++ arm.py
@@ -0,0 +1,37 @@
+#to access files needed to run the code
+import curses
+import setup
+import RoboPiLib as RPL
+#to define motor position
+motor1 = 0
+motor2 = 1
+#to define the 'screen' in front of functions
+screen = curses.initscr()
+curses.halfdelay(3) # How many tenths of a second are waited, from 1 to 255
+#to read key inputs
+key = ''
+#to end loop if 'q' is hit
+while key != ord('q'):
+    #so the key can be read
+    key = screen.getch()
+    screen.clear()
+    screen.addstr('Hit q to quit. Use the W, A, S, and D to test if code works. Detected key: ')
+    #to define what keys preform commands
+    if key != curses.ERR: # This is true if the user pressed something
+        if key == ord('w'):
+            screen.addstr('w key')
+            RPL.servoWrite(motor2, 1000)
+        elif key == ord('s'):
+            screen.addstr('s key')
+            RPL.servoWrite(motor2, 2000)
+        elif key == ord('a'):
+            screen.addstr('a key')
+            RPL.servoWrite(motor1, 1000)
+        elif key == ord('d'):
+            screen.addstr('d key')
+            RPL.servoWrite(motor1, 2000)
+    else:
+        RPL.servoWrite(motor1, 0)
+        RPL.servoWrite(motor2, 0)
+    #to reformat the terminal/end the curses program
+    curses.endwin()
