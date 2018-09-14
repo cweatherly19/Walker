4359ea93ad026faa68e10621eea9cd2eb7bbb19d
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-09-14 10:10:13 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -7,10 +7,13 @@ motor1 = 0
 motor2 = 1
 #to define the 'screen' in front of functions
 screen = curses.initscr()
-curses.halfdelay(3) # How many tenths of a second are waited, from 1 to 255
 #to read key inputs
 key = ''
+#to set motor position
 place = 1000
+place2 = 1000
+RPL.servoWrite(motor2, place)
+RPL.servoWrite(motor1, place2)
 #to end loop if 'q' is hit
 while key != ord('q'):
     #so the key can be read
@@ -18,31 +21,37 @@ while key != ord('q'):
     screen.clear()
     screen.addstr('Hit Q to quit. Use the W, A, S, and D to test if code works. Detected key: ')
     #to define what keys preform commands
-    if key != curses.ERR: # This is true if the user pressed something
-        if key == ord('w'):
-            screen.addstr('w key')
-            place = place + 10
-            if place > 2000:
-                place = 2000
-            if place < 1000:
-                place = 1000
-            RPL.servoWrite(motor2, place)
-        elif key == ord('s'):
-            screen.addstr('s key')
-            place = place - 10
-            if place > 2000:
-                place = 2000
-            if place < 1000:
-                place = 1000
-            RPL.servoWrite(motor2, place)
-        elif key == ord('a'):
-            screen.addstr('a key')
-            RPL.servoWrite(motor1, 1000)
-        elif key == ord('d'):
-            screen.addstr('d key')
-            RPL.servoWrite(motor1, 2000)
-    else:
-        RPL.servoWrite(motor1, 0)
-        RPL.servoWrite(motor2, 0)
+    if key == ord('w'):
+        screen.addstr('w key')
+        place = place + 10
+        if place > 2000:
+            place = 2000
+        if place < 1000:
+            place = 1000
+        RPL.servoWrite(motor2, place)
+    elif key == ord('s'):
+        screen.addstr('s key')
+        place = place - 10
+        if place > 2000:
+            place = 2000
+        if place < 1000:
+            place = 1000
+        RPL.servoWrite(motor2, place)
+    elif key == ord('a'):
+        screen.addstr('a key')
+        place2 = place2 + 10
+        if place2 > 2000:
+            place2 = 2000
+        if place2 < 1000:
+            place2 = 1000
+        RPL.servoWrite(motor1, place2)
+    elif key == ord('d'):
+        screen.addstr('d key')
+        place2 = place2 - 10
+        if place2 > 2000:
+            place2 = 2000
+        if place2 < 1000:
+            place2 = 1000
+        RPL.servoWrite(motor1, place2)
     #to reformat the terminal/end the curses program
     curses.endwin()
