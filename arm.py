1e93acad8278f42951aa7d959bad36a6375e56d3
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-10-01 10:25:09 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -5,8 +5,6 @@ import RoboPiLib as RPL
 #to define motor position
 motor1 = 0
 motor2 = 1
-#to set speed multiplier
-speed = 1
 #to define the 'screen' in front of functions
 screen = curses.initscr()
 #to read key inputs
@@ -27,40 +25,30 @@ while key != ord('q'):
     screen.addstr(str(place))
     screen.addstr(' place2: ')
     screen.addstr(str(place2))
-    screen.addstr(' speed: ')
-    screen.addstr(str(int(speed))
     #to define what keys preform commands
     if key == ord('w'):
         screen.addstr('w key')
-        place = place + (10 * speed)
+        place = place + 10
         if place > 2400:
             place = 2400
         RPL.servoWrite(motor2, int(place))
     elif key == ord('s'):
         screen.addstr('s key')
-        place = place - (10 * speed)
+        place = place - 10
         if place < 400:
            place = 400
         RPL.servoWrite(motor2, int(place))
     elif key == ord('a'):
         screen.addstr('a key')
-        place2 = place2 + (10 * speed)
+        place2 = place2 + 10
         if place2 > 2400:
             place2 = 2400
         RPL.servoWrite(motor1, int(place2))
     elif key == ord('d'):
         screen.addstr('d key')
-        place2 = place2 - (10 * speed)
+        place2 = place2 - 10
         if place2 < 400:
             place2 = 400
-    elif key == ord('e'):
-        speed = 2 * speed
-        if speed > 8:
-            speed = 8
-    elif key == ord('r'):
-        speed = 0.5 * speed
-        if speed < 1:
-            speed = 1
         RPL.servoWrite(motor1, int(place2))
     else:
         screen.addstr('void')
